3ac8a82e15bf040bfa8c2c796d6d2f72be056f5e
yarik <yariks@i.ua>
2020-03-07 10:38:34 +0200

    commit new 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,5 +11,8 @@ public class Main {
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
     }
 }
